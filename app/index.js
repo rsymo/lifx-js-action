193a07ff01ade361d2b281022966b595d9d754e8
Roger Symonds <rsymo@github.com>
2022-11-04 16:13:55 +1100

    making changes with Ambily

--- app/index.js
+++ app/index.js
@@ -22,7 +22,7 @@ async function run()
             const axiosObj = axios.create({
                 baseURL: endpoint,
                 timeout: 1000,
-                headers: {'Authorization': LIFX_TOKEN ,
+                headers: {'Authorization': 'Bearer'+LIFX_TOKEN ,
                 'Content-type': 'application/json'}
             });
 
