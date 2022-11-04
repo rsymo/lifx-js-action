f269660e9abc2f8f3884dd255528dbaff1a44751
Roger Symonds <rsymo@github.com>
2022-11-04 15:38:33 +1100

    making changes with Ambily

--- app/index.js
+++ app/index.js
@@ -6,7 +6,7 @@ let LIFX_BULB_ID = core.getInput('LIFX_BULB_ID')
 let LIFX_TOKEN = core.getInput('LIFX_TOKEN')
 let COLOUR = core.getInput('COLOUR')
 
-let END_POINT = core.getInput('END_POINT)
+//let END_POINT = core.getInput('END_POINT)
 
 
 
@@ -16,23 +16,22 @@ async function run()
             core.notice("calling our action");
 
 
-            const baseURL =“https://api.lifx.com/v1/lights/”+LIFX_BULB_ID;
+            const baseURL ='https://api.lifx.com/v1/lights/'+LIFX_BULB_ID;
             let response = await axios({
               baseURL: endpoint,
-              url: ‘/state’,
-              method: ‘post’,
+              url: '/state',
+              method:'post',
               headers: {
-                ‘Bearer’: LIFX_TOKEN ,
-                ‘Content-type’: ‘application/json’,
-                ‘X-ClientTraceId’: uuidv4().toString()
+                'Bearer': LIFX_TOKEN ,
+                'Content-type': 'application/json',
               },
               data: [{
-                ‘power’:‘on’,
-                ‘brightness’: ‘1.0’,
-                 ‘color’: COLOUR,
-                 ‘fast’: true
+                'power':'on',
+                'brightness': '1.0',
+                 'color': COLOUR,
+                 'fast': true
               }],
-              responseType: ‘json’
+              responseType: 'json'
             });
 
 
@@ -43,6 +42,10 @@ async function run()
         }
     
 };
+
+run();
+
+
 
 // Initiate Light - set it blue
 
